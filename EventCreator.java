b9e012879047d4ddfe5e76d969d88b904d0bb339
GabrieleOx <ossolagabriele@gmail.com>
2025-05-14 19:59:54 +0200

    Aggiunta action part per invocazione inserimento eventi

--- EventCreator.java
+++ EventCreator.java
@@ -1,8 +1,10 @@
 
 import java.awt.Color;
 import java.awt.Font;
+import java.util.ArrayList;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -10,16 +12,22 @@ import javax.swing.WindowConstants;
 
 public class EventCreator {
     public static void main(String[] args) {
+        ArrayList<Evento> events = new ArrayList<>();
+        final Integer [] nEvents = new Integer[20];
+
+        for (int i = 0; i < 19; i++) nEvents[i] = i+2;
+
         JFrame baseWindow = new JFrame("EventCreator");
         JLabel nomeFileLabel = new JLabel("Nome file:"), titoloLabel = new JLabel("Quanti eventi vuoi inserire ?");
-        JTextField nomeFileTextField = new JTextField("GenericEvent"), numberEventsTextField = new JTextField("2");
+        JTextField nomeFileTextField = new JTextField("GenericEvent");
+        JComboBox numberEventsTextField = new JComboBox<>(nEvents);
         JCheckBox singleEventCheckBox = new JCheckBox(":Evento Singolo"), multipleEventCheckBox = new JCheckBox(":Evento Multiplo");
-        JButton insertButton = new JButton("Inserisci");
+        JButton insertButton = new JButton("Inserisci"), endButton = new JButton("Fine Inserimento");
 
         //Graphical part
 
         baseWindow.setResizable(false);
-        baseWindow.setSize(500, 500);
+        baseWindow.setSize(500, 600);
         baseWindow.getContentPane().setBackground(Color.gray);
         baseWindow.setLayout(null);
         baseWindow.add(nomeFileLabel);
@@ -29,6 +37,7 @@ public class EventCreator {
         baseWindow.add(multipleEventCheckBox);
         baseWindow.add(numberEventsTextField);
         baseWindow.add(insertButton);
+        baseWindow.add(endButton);
 
         nomeFileLabel.setBounds(60, 20, 110, 40);
         nomeFileLabel.setFont(new Font("Verdana", Font.PLAIN, 20));
@@ -36,17 +45,18 @@ public class EventCreator {
         nomeFileTextField.setFont(new Font("Verdana", Font.PLAIN, 20));
         nomeFileTextField.setBackground(new Color(180, 180, 180));
 
-        titoloLabel.setBounds(40, 100, 410, 40);
+        titoloLabel.setBounds(60, 100, 410, 40);
         titoloLabel.setFont(new Font("Verdana", Font.BOLD, 25));
         titoloLabel.setForeground(new Color(99, 45, 45));
 
-        //First col:
+        //First col
 
         singleEventCheckBox.setBounds(30, 250, 200, 30);
         singleEventCheckBox.setFont(new Font("Verdana", Font.PLAIN, 20));
         singleEventCheckBox.setBackground(Color.gray);
+        singleEventCheckBox.setSelected(true);
 
-        //Second col:
+        //Second col
 
         multipleEventCheckBox.setBounds(260, 210, 200, 30);
         multipleEventCheckBox.setFont(new Font("Verdana", Font.PLAIN, 20));
@@ -55,12 +65,56 @@ public class EventCreator {
         numberEventsTextField.setBounds(260, 300, 200, 30);
         numberEventsTextField.setFont(new Font("Verdana", Font.PLAIN, 20));
         numberEventsTextField.setBackground(new Color(180, 180, 180));
+        numberEventsTextField.setEnabled(false);
 
-        //Bottom:
+        //Bottom
 
         insertButton.setBounds(140, 380, 200, 50);
         insertButton.setFont(new Font("Verdana", Font.BOLD, 20));
         insertButton.setBackground(new Color(186, 130, 130));
+
+        endButton.setBounds(120, 480, 240, 50);
+        endButton.setFont(new Font("Verdana", Font.BOLD, 20));
+        endButton.setBackground(Color.red);
+
+        //Action part
+
+        insertButton.addActionListener(ActionListener ->{
+            if(nomeFileTextField.getText().equals(""))
+                nomeFileTextField.setText("GenericEvent");
+
+            if(singleEventCheckBox.isSelected())
+                events.add(new Evento("Nuovo Evento", baseWindow));
+            else{
+                int nEventi = (int) numberEventsTextField.getSelectedItem();
+                for(int i = 0; i < nEventi; i++)
+                    events.add(new Evento("Nuovo Evento " + (i+1), baseWindow));
+            }
+        });
+
+        singleEventCheckBox.addActionListener(ActionListener ->{
+            if(singleEventCheckBox.isSelected()){
+                multipleEventCheckBox.setSelected(false);
+                numberEventsTextField.setEnabled(false);
+            }else {
+                multipleEventCheckBox.setSelected(true);
+                numberEventsTextField.setEnabled(true);
+            }
+        });
+
+        multipleEventCheckBox.addActionListener(ActionLister ->{
+            if(multipleEventCheckBox.isSelected()){
+                singleEventCheckBox.setSelected(false);
+                numberEventsTextField.setEnabled(true);
+            }else {
+                singleEventCheckBox.setSelected(true);
+                numberEventsTextField.setEnabled(false);
+            }   
+        });
+
+        endButton.addActionListener(ActionListener -> {
+            //Stampa su file
+        });
 
         baseWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         baseWindow.setVisible(true);
